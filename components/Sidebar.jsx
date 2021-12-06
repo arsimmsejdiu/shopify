1fc11c2d0425b79daa0e1616111c546cce6129f5
arsimmsejdiu <arsimmsejdiu@gmail.com>
2021-12-06 19:24:44 +0100

    added items to the sidebar

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -3,11 +3,13 @@ import {
   SearchIcon,
   LibraryIcon,
   PlusCircleIcon,
+  RssIcon,
+  HeartIcon
 } from "@heroicons/react/outline";
 
 function Sidebar() {
   return (
-    <div>
+    <div className="text-gray-500 p-5 text-sm border-r border-gray-900">
       <div>
         <button className="flex items-center space-x-2 hover:text-white">
           <HomeIcon className="w-5 h-5" />
@@ -21,6 +23,21 @@ function Sidebar() {
           <LibraryIcon className="w-5 h-5" />
           <p>Your Library</p>
         </button>
+        <hr className="border-t-[0.1px] border-gray-900" />
+
+        <button className="flex items-center space-x-2 hover:text-white">
+          <PlusCircleIcon className="w-5 h-5" />
+          <p>Create Playlist</p>
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <RssIcon className="w-5 h-5" />
+          <p>Your Episodes</p>
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <HeartIcon className="w-5 h-5" />
+          <p>Your Favorite</p>
+        </button>
+        <hr className="border-t-[0.1px] border-gray-900" />
       </div>
     </div>
   );
